345c0cd7c5b367dabea6c885378896a789ea6870
Pratik Sachdeva <pratik.sachdeva@berkeley.edu>
2025-12-29 22:49:49 -0800

    set up package

---
+++ src/legal_attitudes/__init__.py
@@ -0,0 +1,22 @@
+from .utils import (
+    EXPERIMENTS_DIR,
+    PROMPTS_DIR,
+    RESULTS_DIR,
+    ROOT,
+    SCRIPTS_DIR,
+    find_root,
+    result_exists,
+    save_result,
+)
+
+__all__ = [
+    "find_root",
+    "ROOT",
+    "PROMPTS_DIR",
+    "EXPERIMENTS_DIR",
+    "SCRIPTS_DIR",
+    "RESULTS_DIR",
+    "result_exists",
+    "save_result",
+]
+
